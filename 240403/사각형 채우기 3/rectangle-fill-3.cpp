16f7210e036266fdc380688dce4c078f5ef62375
korean-owl <91177645+korean-owl@users.noreply.github.com>
2024-04-03 21:47:38 +0900

    Title: 사각형 채우기 3, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240403/사각형 채우기 3/rectangle-fill-3.cpp
+++ 240403/사각형 채우기 3/rectangle-fill-3.cpp
@@ -14,7 +14,7 @@ int main() {
     for (int i=2 ; i <=n ; i++){
                dp[i] = (dp[i - 1] * 2 + dp[i - 2] * 3) ;
            for(int j=i-3; j>=0;j--){
-                dp[i] = (dp[i] + dp[j] * 2);
+                dp[i] = (dp[i] + dp[j] * 2) % 1000000007;
             }
         dp[i] %= 1000000007;
     }
