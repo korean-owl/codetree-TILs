dbd598d34d20f3cc651304e1b873e165564a3341
korean-owl <91177645+korean-owl@users.noreply.github.com>
2024-06-23 14:42:28 +0900

    Title: 2개의 사탕, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240623/2개의 사탕/two-candies.cpp
+++ 240623/2개의 사탕/two-candies.cpp
@@ -1,120 +1,122 @@
-#include <iostream>
-#include <queue>
-#include <cmath>
+#include<iostream>
+#include<queue>
 using namespace std;
 
 struct INFO {
-    int rx, ry, bx, by, count;
+	int rx, ry, bx, by, count;
 };
-INFO start; // 시작 위치
-char map[10][11]; // 맨 끝 개행 문자 포함
+INFO start; //시작 위치
+char map[12][12]; //맨끝개행문자
 int n, m;
 int answer;
-
 bool CanGo(int x, int y) {
-    return map[x][y] != '#' && map[x][y] != '0';
+	return map[x][y] != '#' && map[x][y] != 'O';
 }
 
 void InPut() {
-    cin >> n >> m;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cin >> map[i][j];
-            if (map[i][j] == 'R') {
-                start.ry = i;
-                start.rx = j;
-            } else if (map[i][j] == 'B') {
-                start.bx = i;
-                start.by = j;
-            }
-        }
-    }
-    start.count = 0;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < m; j++) {
+			cin >> map[i][j];
+			if (map[i][j] == 'R') {
+				start.rx = j;
+				start.ry = i;
+			}
+			else if (map[i][j] == 'B') {
+				start.bx = j;
+				start.by = i;
+			}
+		}
+	start.count = 0;
 }
 
 int BFS() {
-    const int dy[] = {-1, 1, 0, 0};
-    const int dx[] = {0, 0, -1, 1};
+	const int dy[] = { -1, 1, 0, 0 };
+	const int dx[] = { 0, 0, -1, 1 };
 
-    bool visited[10][10][10][10] = {false};
-    queue<INFO> q;
-    q.push(start);
-    visited[start.ry][start.rx][start.by][start.bx] = true;
+	int visited[10][10][10][10] = { 0, };
+	queue<INFO> q;
+	q.push(start);
+	visited[start.ry][start.rx][start.by][start.bx] = true;
 
-    int ans = -1;
-    while (!q.empty()) {
-        INFO cur = q.front();
-        q.pop();
+	int ans = -1;
+	while (!q.empty()) {
+		INFO cur = q.front();
+		q.pop();
 
-        if (cur.count > 10) break;
-        if (map[cur.ry][cur.rx] == 'O' && map[cur.by][cur.bx] != 'O') {
-            ans = cur.count;
-            break;
-        }
+		if (cur.count > 10) break;
+		if (map[cur.ry][cur.rx] == 'O' && map[cur.by][cur.bx] != 'O') {
+			ans = cur.count;
+			break;
+		}
 
-        for (int i = 0; i < 4; i++) {
-            int next_ry = cur.ry;
-            int next_rx = cur.rx;
-            int next_by = cur.by;
-            int next_bx = cur.bx;
+		for (int i = 0; i < 4; i++) {
+			int nry = cur.ry;
+			int nrx = cur.rx;
+			int nby = cur.by;
+			int nbx = cur.bx;
 
-            // red 구슬 한 방향으로 이동
-            while (true) {
-                if (map[next_ry][next_rx] != '#' && map[next_ry][next_rx] != 'O') {
-                    next_ry += dy[i];
-                    next_rx += dx[i];
-                } else {
-                    if (map[next_ry][next_rx] == '#') {
-                        next_ry -= dy[i];
-                        next_rx -= dx[i];
-                    }
-                    break;
-                }
-            }
+			while (true) {
+				if (CanGo(nry + dy[i], nrx + dx[i])) {
+					nry += dy[i];
+					nrx += dx[i];
+				}
+				else {
+					if (map[nry + dy[i]][nrx + dx[i]] == '#') {
+						nry -= dy[i];
+						nrx -= dx[i];
+					}
+					break;
+				}
+			}
 
-            // blue 구슬 한 방향으로 이동
-            while (true) {
-                if (map[next_by][next_bx] != '#' && map[next_by][next_bx] != 'O') {
-                    next_by += dy[i];
-                    next_bx += dx[i];
-                } else {
-                    if (map[next_by][next_bx] == '#') {
-                        next_by -= dy[i];
-                        next_bx -= dx[i];
-                    }
-                    break;
-                }
-            }
+			while (true) {
+				if (CanGo(nby + dy[i], nbx + dx[i])) {
+					nby += dy[i];
+					nbx += dx[i];
+				}
+				else {
+					if (map[nby + dy[i]][nbx + dx[i]] == '#') {
+						nby -= dy[i];
+						nbx -= dx[i];
+					}
+					break;
+				}
+			}
 
-            // red와 blue가 겹칠 경우
-            if (next_ry == next_by && next_rx == next_bx) {
-                if (map[next_ry][next_rx] != 'O') {
-                    int red_dist = abs(next_ry - cur.ry) + abs(next_rx - cur.rx);
-                    int blue_dist = abs(next_by - cur.by) + abs(next_bx - cur.bx);
+			if (nry == nby && nrx == nbx) {
+				if (map[nry][nrx] != 'O') {
+					int redDist = abs(nrx - cur.rx) + abs(nry - cur.ry);
+					int blueDist = abs(nbx - cur.bx) + abs(nby - cur.by);
+					if (blueDist > redDist) {
+						nby -= dy[i];
+						nbx -= dx[i];
+					}
+					else {
+						nrx -= dy[i];
+						nry -= dx[i];
+					}
+				}
+			}
 
-                    if (blue_dist > red_dist) {
-                        next_by -= dy[i];
-                        next_bx -= dx[i];
-                    } else {
-                        next_ry -= dy[i];
-                        next_rx -= dx[i];
-                    }
-                }
-            }
-
-            if (!visited[next_ry][next_rx][next_by][next_bx]) {
-                visited[next_ry][next_rx][next_by][next_bx] = true;
-                INFO next = {next_rx, next_ry, next_bx, next_by, cur.count + 1};
-                q.push(next);
-            }
-        }
-    }
-    return ans;
+			if (!visited[nry][nrx][nby][nbx]) {
+				visited[nry][nrx][nby][nbx] = true;
+				INFO next;
+				next.ry = nry;
+				next.rx = nrx;
+				next.by = nby;
+				next.bx = nbx;
+				next.count = cur.count + 1;
+				q.push(next);
+			}
+		}
+	}
+	return ans;
 }
 
 int main() {
-    InPut();
-    answer = BFS();
-    cout << answer << endl;
-    return 0;
+	InPut();
+	answer = BFS();
+	cout << answer << endl;
+	return 0;
 }
